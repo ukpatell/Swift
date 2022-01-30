99de62985e7de86abb6a6f81a924e3d7625ab093
ukpatell <umangpatelr4@gmail.com>
2022-01-30 02:03:13 -0500

    Playground for Swift

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var greeting = "Hello, playground"
+ 
+var number = 123
+
+print(greeting,number)
+
+let boss = "Mr. Boss"   // Constant Variables
+
+print(boss, ", says ", greeting)
