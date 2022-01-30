81b36e56c9f3089a6bfeedf73c22c563ea75795f
ukpatell <umangpatelr4@gmail.com>
2022-01-30 02:30:14 -0500

    Datatypes

---
+++ Datatype.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var first_name:String = "Umang"             // Assign data type th
+var last_name = "Patel"
+//last_name = 1                               // If you don't assign data type, swift will pick up the first entry
+
+var a:Int,b:Int,c:Int
+a = 2;b=3;c=5                               // Consecutive lines can be separated by ';'
+
+print(a*b*c)
+pow(2,3)
+
+
+
